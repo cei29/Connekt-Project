79d617df9f1545c47f5bef9e51be0ec54a6a2546
cei29 <cei29@outlook.com>
2021-06-01 22:24:49 -0300

    User page almost done, scroll down button needs some adjusting

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,16 @@
-/* SPINNER-LOADER */
+/* spinner-loader */
 const loader = document.querySelector('.spinner-border')
 
 window.onload = () => {
     loader.style.display = 'none'
 }
+
+/* edit-user */
+const box = document.querySelectorAll('.edit-user .form-box, .edit-user input')
+
+box.forEach(element => {
+    element.onclick = () => {
+        element.style.backgroundColor = '#fff';
+        element.style.cursor = 'text'
+    }
+});
